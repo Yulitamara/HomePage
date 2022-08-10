d7e3ffb2089558909177556f6a6237e8d91bcc71
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-08-10 16:22:20 +0300

    10/8/22 -2

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,6 @@
 // SIDEBAR
 
-$(document).ready(function () {
-  // toggle sub menus
-  $(".sub-btn").click(function () {
-    $(this).next(".sub-menu").slideToggle();
-    $(this).find(".dropdown").toggleClass("rotate");
-  });
-
-  // expand and collapse the sidebar
-  //   $(".menu-btn").click(function () {
-  //     $(".sidebar").addClass("active-sidebar");
-  //     $(".main-content").addClass("active-main-content");
-  //     $(".menu-btn").css("visibility", "hidden");
-  //   });
-
-  //   $(".nav-close-btn").click(function () {
-  //     $(".sidebar").removeClass("active-sidebar");
-  //     $(".main-content").removeClass("active-main-content");
-  //     $(".menu-btn").css("visibility", "visible");
-  //   });
-});
-
 // Debit Plan Tabs
-
 $(function () {
   $("#tabs").tabs();
 });
