f153e29c4277e12570cbf26f35a22e6e12149481
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-08-22 10:02:31 +0300

    links expand

--- JS/app.js
+++ JS/app.js
@@ -24,54 +24,77 @@ $(function () {
 $(function () {
   $(".close-section").click(function () {
     $(".tab-content").hide();
-
     $(".link").removeClass("active-link");
   });
 });
 
 // Expand Section
 $(".links-expanded").hide();
-$(".big-close-icon").hide();
+// $(".big-close-btn").hide();
 
-$(".expand-btn, .links-unexpand").click(function () {
+$(".big-expand-btn, .links-unexpand").click(function () {
   $(".links-unexpand").addClass("links-unexpand-clicked");
   $(".expand-section").addClass("expand-section-clicked");
-  $(".links-unexpand-tag").addClass("no-background");
-  $(".type-header").addClass("display-none");
-  $("#links-unexpand-1").html("Links");
-  $("#links-unexpand-2").html("Stages");
-  $("#links-unexpand-3").html("Debit Plans");
-  $("#links-unexpand-4").html("Related");
-  $(".big-close-icon").show();
-});
-
-$(".big-close-icon").click(function () {
-  $(".big-close-icon").hide();
-  $(".links-unexpand").removeClass("links-unexpand-clicked");
-  $(".expand-section").removeClass("expand-section-clicked");
-  $(".links-unexpand-tag").removeClass("no-background");
-  $(".type-header").removeClass("display-none");
 });
 
 $("#expand-btn-1, #links-unexpand-1").click(function () {
   $("#links-unexpand-1").toggleClass("links-unexpand-focus");
   $("#links-expanded-1").slideToggle();
-  $("body").scrollTo("#expand-btn-1");
+  $("#expand-btn-1").html() == "close"
+    ? $(this).html("expand_more")
+    : $(this).html("close");
+
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#links-expanded-1").offset().top,
+    },
+    1000
+  );
 });
 
 $("#expand-btn-2, #links-unexpand-2").click(function () {
   $("#links-unexpand-2").toggleClass("links-unexpand-focus");
   $("#links-expanded-2").slideToggle();
+  $("#expand-btn-2").html() == "close"
+    ? $(this).html("expand_more")
+    : $(this).html("close");
+
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#links-expanded-2").offset().top,
+    },
+    1000
+  );
 });
 
 $("#expand-btn-3, #links-unexpand-3").click(function () {
   $("#links-unexpand-3").toggleClass("links-unexpand-focus");
   $("#links-expanded-3").slideToggle();
+  $("#expand-btn-3").html() == "close"
+    ? $(this).html("expand_more")
+    : $(this).html("close");
+
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#links-expanded-3").offset().top,
+    },
+    1000
+  );
 });
 
 $("#expand-btn-4, #links-unexpand-4").click(function () {
   $("#links-unexpand-4").toggleClass("links-unexpand-focus");
   $("#links-expanded-4").slideToggle();
+  $("#expand-btn-4").html() == "close"
+    ? $(this).html("expand_more")
+    : $(this).html("close");
+
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#links-expanded-4").offset().top,
+    },
+    1000
+  );
 });
 
 if ($(window).width() < 900) {
