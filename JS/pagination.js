3d1f94eb9745b4d6cc166d556f7af0c81871132c
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-10-18 13:07:20 +0300

    scroll

--- JS/pagination.js
+++ JS/pagination.js
@@ -1,17 +1,14 @@
+// const btnLeft = document.querySelector(".table-btn--left");
+// const btnRight = document.querySelector(".table-btn--right");
+// const box = document.getElementsByClassName("box");
 const slider = document.querySelector(".slider");
-const btnLeft = document.querySelector(".table-btn--left");
-const btnRight = document.querySelector(".table-btn--right");
-const box = document.getElementsByClassName("box");
+// console.log(slider);
 
-function scrollParent(event, offset) {
+function scrollSlider(event, offset) {
   // let parentElm = document.querySelector(".slider");
   slider.scrollBy({
     top: 0,
-    left: 100 * offset,
+    left: 170 * offset,
     behavior: "smooth",
   });
-}
-
-for (let i = 0; i < slider.length; i++) {
-  console.log(slider[i]);
 }
